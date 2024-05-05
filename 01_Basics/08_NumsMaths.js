99feaf954195df45c1d4226b3e00f92753f1a098
safi-thecoder <m.safi.ullah@outlool.com>
2024-05-05 20:09:51 +0500

    Enhanced through desktops

--- 01_Basics/08_NumsMaths.js
+++ 01_Basics/08_NumsMaths.js
@@ -27,4 +27,4 @@ let low = 5
 let high = 10
 
 console.log(Math.floor(Math.random()*(high-low+1)+low))
-// To get Value between low and high numbers
+// To get only values between low and high numbers
