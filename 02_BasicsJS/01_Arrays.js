964630204733120174051bc7971ed341c5c953a7
Muhammad Safiullah Khan <145552372+safi-thecoder@users.noreply.github.com>
2024-05-10 13:36:38 +0000

    Enhanced some Arr Methods

--- 02_BasicsJS/01_Arrays.js
+++ 02_BasicsJS/01_Arrays.js
@@ -2,23 +2,20 @@
 const Arr = [1, 2, 3, 4, 5, "true", "safi"]
 const Arr2 = new Array("Pakistan", "India", "China")
 
-// // Arrays Methods
-// Arr.push(69) // Adds Elements to end of arr
-// Arr.pop() // Removes Elements from end of arr
+// Arrays Methods
+Arr.push(69) // Adds Elements to end of arr
+Arr.pop() // Removes Elements from end of arr
 
-// Arr.unshift(123) // Adds Elements to start of arr
-// Arr.shift() // Removes Elemets from start of arr
+Arr.unshift(123) // Adds Elements to start of arr
+Arr.shift() // Removes Elemets from start of arr
 
-// console.log(Arr.includes("safi")); // Returns 1 if element is available
+console.log(Arr.includes("safi")); // Returns 1 if element is available
 
-// console.log(Arr.indexOf("safi")) // Returns Index of given data
+console.log(Arr.indexOf("safi")) // Returns Index of given data
 
-// const Arr3 = Arr.join(); // Converts Arryas into Strings
+const Arr3 = Arr.join(); // Converts Arryas into Strings
 
-
-// ----------------------
-
-// Slice
+// Slice and Splice
 
 let na1 = Arr.slice(0,2); // 2nd parameter element will be exculded and original arr will stay same
 console.table([Arr, na1]);
