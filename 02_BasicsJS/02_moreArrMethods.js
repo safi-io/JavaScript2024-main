9e28cf2036d70a6df67d4fbdec51845594628ffb
safi-thecoder <m.safi.ullah@outlool.com>
2024-05-11 22:37:33 +0500

    Learned more Array methods

---
+++ 02_BasicsJS/02_moreArrMethods.js
@@ -0,0 +1,9 @@
+let asia_countries = ["Pakistan", "India", "China", "Bangladesh"]
+let eur_countries = ["Germany","Netherlands", "Spain", "France"]
+
+// asia_countries.push(eur_countries) // It will add full array as single element
+
+// console.log(asia_countries[4][3]) // Expected Output:- France
+
+console.log(asia_countries.concat(eur_countries)) // It will normally join two arrays
+
