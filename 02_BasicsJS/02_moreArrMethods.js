4fccd72372d0bf282f319fd47b33f7be3cd96843
safi-thecoder <m.safi.ullah@outlool.com>
2024-05-11 22:57:38 +0500

    Completed Almost Basics Arr Methods

--- 02_BasicsJS/02_moreArrMethods.js
+++ 02_BasicsJS/02_moreArrMethods.js
@@ -1,9 +1,21 @@
 let asia_countries = ["Pakistan", "India", "China", "Bangladesh"]
 let eur_countries = ["Germany","Netherlands", "Spain", "France"]
 
-// asia_countries.push(eur_countries) // It will add full array as single element
+// CONCAT
+// console.log(asia_countries.concat(eur_countries)) // It will normally join two arrays
 
+// asia_countries.push(eur_countries) // It will add full array as single element
 // console.log(asia_countries[4][3]) // Expected Output:- France
 
-console.log(asia_countries.concat(eur_countries)) // It will normally join two arrays
+// Spread Operator
 
+let globe = [...asia_countries, ...eur_countries];
+// console.log(globe);
+
+// Flat Method
+
+// console.log(asia_countries.flat(Infinity)) // Returns all sub-elements, infinity means to every level.
+
+
+
+// Is, From, of
