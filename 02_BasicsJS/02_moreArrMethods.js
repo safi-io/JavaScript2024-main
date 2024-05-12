7975feabef4aff16923d09b527d02e475052f070
safi-thecoder <m.safi.ullah@outlool.com>
2024-05-12 09:22:42 +0500

    Done with Basic Arrays

--- 02_BasicsJS/02_moreArrMethods.js
+++ 02_BasicsJS/02_moreArrMethods.js
@@ -2,20 +2,28 @@ let asia_countries = ["Pakistan", "India", "China", "Bangladesh"]
 let eur_countries = ["Germany","Netherlands", "Spain", "France"]
 
 // CONCAT
-// console.log(asia_countries.concat(eur_countries)) // It will normally join two arrays
+console.log(asia_countries.concat(eur_countries)) // It will normally join two arrays
 
-// asia_countries.push(eur_countries) // It will add full array as single element
-// console.log(asia_countries[4][3]) // Expected Output:- France
+asia_countries.push(eur_countries) // It will add full array as single element
+console.log(asia_countries[4][3]) // Expected Output:- France
 
 // Spread Operator
 
 let globe = [...asia_countries, ...eur_countries];
-// console.log(globe);
+console.log(globe);
 
 // Flat Method
 
-// console.log(asia_countries.flat(Infinity)) // Returns all sub-elements, infinity means to every level.
+console.log(asia_countries.flat(Infinity)) // Returns all sub-elements, infinity means to every level.
 
+// IS Array Method
+console.log(Array.isArray(asia_countries)) // Returns True if Argument is Array
 
+// From Array Method
+let greet = "morning"
+let total = Array.from(greet); // Basically Converting String into a array
+console.log(total)
 
-// Is, From, of
+// OF Arr Method
+let fnlarr = Array.of(globe, greet, total) // Creates Arrays from Diffrent DataTypes
+console.log(fnlarr)
