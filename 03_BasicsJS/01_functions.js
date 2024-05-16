eacfc96cf6853e833e4fa254d146fb71c27e4f41
safi-thecoder <m.safi.ullah@outlool.com>
2024-05-16 08:40:40 +0500

    Done with Functions

--- 03_BasicsJS/01_functions.js
+++ 03_BasicsJS/01_functions.js
@@ -1,4 +1,5 @@
     // Making a function program that accepts two parameters and calculates their sum!
+
 let num1 = 20, num2 = 30;
 function add(num1, num2) { // Parameters
     return num1+num2;
@@ -17,3 +18,24 @@ function isLoggedIn(username) { // Default Value can be username = "user"
 }
 
 let result2 = isLoggedIn("safi")
+
+    // Rest Operator in JS
+
+function returnNums(num1, ...nums) { // ... is used when we are unaware of how many arguments are being passed
+    return nums;
+}
+
+let result3 = returnNums(5,4,5,3); // First argument will go in num1 and other in nums.
+
+    // Passing Objects in Functions
+
+const new_laptop = {
+    model : "HP",
+    price : 999
+}
+
+function objectHandler(anyObj) {
+    return `Model of the Laptop is ${anyObj.model} and Price of the laptop is ${anyObj.price}`
+}
+
+let result4 = objectHandler(new_laptop)
