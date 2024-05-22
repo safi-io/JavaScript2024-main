e026393eb961e6cbc238f4e0c83a8ad257b8d803
safi-thecoder <m.safi.ullah@outlool.com>
2024-05-22 16:14:59 +0500

    Will Start For Each Soon

---
+++ 05_Iterations/04_forIn.js
@@ -0,0 +1,11 @@
+// a for...in loop in JavaScript lets you loop through all the properties inside an object one by one.
+
+const new_laptop = {
+    price : 40,
+    color : "White",
+    company : "Apple"
+}
+
+for (const key in new_laptop) {
+   console.log(`Key is ${key} and value is ${new_laptop[key]}`)
+}
