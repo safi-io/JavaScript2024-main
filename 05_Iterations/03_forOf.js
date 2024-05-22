7142c2ee0776446744d3d7cc3e999f1a85bd3931
safi-thecoder <m.safi.ullah@outlool.com>
2024-05-22 16:04:46 +0500

    For Of Done

--- 05_Iterations/03_forOf.js
+++ 05_Iterations/03_forOf.js
@@ -1,3 +1,6 @@
+
+    // For Of is used with iterables like arrays to iterate over values.
+
 let numbers = [1,2,3,4,4,5];
 
 for(let num of numbers) {
@@ -7,8 +10,21 @@ for(let num of numbers) {
 let greetings = "Hello World!";
 
 for(let greet of greetings) {
-    if(greet == " ") {
-        continue;
-    }
-    console.log(greet)
+    // if(greet == " ") {
+    //     continue;
+    // }
+    // console.log(greet)
+}
+
+    // Map
+
+const fruits = new Map(); // Maps are also iterable through for Of
+// They have uniques keys and remember their order
+
+fruits.set("apples", 500);
+fruits.set("bananas", 400);
+fruits.set("mangos", 300);
+
+for(let [fruit, values] of fruits) {
+    console.log(fruit, ":" , values)
 }
