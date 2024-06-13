618f60e54047f32a951ef6b513bf4b09e84866b1
safi-thecoder <m.safi.ullah@outlool.com>
2024-06-13 19:26:16 +0500

    Done with Theory of Async

---
+++ 09_Async/01_Theory.js
@@ -0,0 +1,17 @@
+// By Default, JS is Synchoronous and Single Threaded
+
+// Blocking Code
+// ----- Blocks the Flow of Program
+// ----- Reads File Sync
+
+// Non-Blocking Code
+// ----- Doesnt not Blocks the Flow of Program
+// ----- Reads File Async
+
+// Task Queue
+// ----- To handle asynchronous tasks (like fetching data from a server), JS uses a task queue.
+
+// When Set timeout and Set interval are used
+// that specific code goes into Task Queue
+
+// fetch() --> High Priority Task Queue
