59fb77611b09b81d2b3a5312191b98d5bc3ea820
safi-thecoder <m.safi.ullah@outlool.com>
2024-06-15 11:09:02 +0500

    Learning Asycn Nature

--- 09_Async/01_Theory.js
+++ 09_Async/01_Theory.js
@@ -1,17 +1,19 @@
-// By Default, JS is Synchoronous and Single Threaded
+/*
+By Default, JS is Synchoronous and Single Threaded
 
-// Blocking Code
-// ----- Blocks the Flow of Program
-// ----- Reads File Sync
+Blocking Code
+----- Blocks the Flow of Program
+----- Reads File Sync
 
-// Non-Blocking Code
-// ----- Doesnt not Blocks the Flow of Program
-// ----- Reads File Async
+Non-Blocking Code
+----- Doesnt not Blocks the Flow of Program
+----- Reads File Async
 
-// Task Queue
-// ----- To handle asynchronous tasks (like fetching data from a server), JS uses a task queue.
+Task Queue
+----- To handle asynchronous tasks (like fetching data from a server), JS uses a task queue.
 
-// When Set timeout and Set interval are used
-// that specific code goes into Task Queue
+When Set timeout and Set interval are used
+that specific code goes into Task Queue.
 
-// fetch() --> High Priority Task Queue
+fetch() --> High Priority Task Queue
+*/
